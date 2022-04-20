b22703fa17c01756dc7e87c0f6500664061efc1c
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-04-20 13:48:05 +0700

    update code

--- src/insertion-sort/main.go
+++ src/insertion-sort/main.go
@@ -1,7 +1,3 @@
-// Kelas	: MM4
-// Nama		: REZA IRFAN WIJAYA
-// NIM		: 19102149
-
 package main
 
 import (
@@ -15,7 +11,7 @@ import (
 // ambil data dari folder data
 func ReadData(filename string) []int {
 	// ambil path dari file
-	data, err := ioutil.ReadFile("../data/" + filename)
+	data, err := ioutil.ReadFile("../../data/" + filename)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -32,46 +28,6 @@ func ReadData(filename string) []int {
 	return res
 
 }
-
-// =================== SELECTION SORT =======================
-
-// function selection sort dengan parameter array, awal, akhir dan balikan berupa slice int
-func SelectionSort(arr []int, i, j int) []int {
-	// jika awal lebih kecil dari akhir
-	if i < j {
-		// lakukan partisi pada array
-		p := Partisi3(arr, i, j)
-		// urutkan array dengan partisi yang telah di lakukan
-		arr = SelectionSort(arr, i, p-1)
-		// urutkan array dengan partisi yang telah di lakukan
-		arr = SelectionSort(arr, p+1, j)
-	}
-	// kembalikan array
-	return arr
-}
-
-// function artisi dengan parameter array, awal, akhir denagan balikan berupa int
-func Partisi3(arr []int, i, j int) int {
-	// looping semua data pada array
-	for k := i + 1; k <= j; k++ {
-		// tetap kan nilai minimal
-		nilaiMin := i
-		// jika array k lebih kecil dari array k+1
-		if arr[k] < arr[nilaiMin] {
-			// nilai minimal di ganti dengan k
-			nilaiMin = k
-		}
-		// lakukan pertukaran posisi
-		arr[nilaiMin], arr[i] = arr[i], arr[nilaiMin]
-	}
-
-	// kembalikan nilai minimal
-	return i
-}
-
-// =================== END SELECTION SORT =====================
-
-// =================== INSERTION SORT =========================
 
 // function insertion sort dengan parameter array, awal, akhir dan balikan berupa slice int
 func InsertionSort(arr []int, i, j int) []int {
@@ -108,31 +64,9 @@ func Merge(arr []int, i, k, j int) []int {
 	return arr
 }
 
-// =================== END INSERTION SORT =====================
-
 func main() {
-
 	// penghitung waktu
 	start := time.Now()
-
-	// my data
-	data := ReadData("Number.txt")
-
-	fmt.Printf("\n==============================================================\n")
-	fmt.Println("====================== SELECTION SORT ========================")
-	fmt.Printf("==============================================================\n")
-
-	// print data sebelum diurutkan
-	fmt.Printf("Data sebelum diurutkan: %v\n", data)
-
-	// urutkan data
-	data = SelectionSort(data, 0, len(data)-1)
-
-	// print data setelah diurutkan
-	fmt.Printf("Data setelah diurutkan: %v\n\n\n\n", data)
-
-	// durasi ekseskusi program
-	fmt.Printf("Durasi eksekusi program: %v Detik \n", time.Since(start).Seconds())
 
 	fmt.Printf("\n==============================================================\n")
 	fmt.Println("====================== INSERTION SORT ========================")
--- src/selection-sort/main.go
+++ src/selection-sort/main.go
@@ -1,7 +1,3 @@
-// Kelas	: MM4
-// Nama		: REZA IRFAN WIJAYA
-// NIM		: 19102149
-
 package main
 
 import (
@@ -15,7 +11,7 @@ import (
 // ambil data dari folder data
 func ReadData(filename string) []int {
 	// ambil path dari file
-	data, err := ioutil.ReadFile("../data/" + filename)
+	data, err := ioutil.ReadFile("../../data/" + filename)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -30,10 +26,7 @@ func ReadData(filename string) []int {
 	}
 
 	return res
-
 }
-
-// =================== SELECTION SORT =======================
 
 // function selection sort dengan parameter array, awal, akhir dan balikan berupa slice int
 func SelectionSort(arr []int, i, j int) []int {
@@ -69,49 +62,7 @@ func Partisi3(arr []int, i, j int) int {
 	return i
 }
 
-// =================== END SELECTION SORT =====================
-
-// =================== INSERTION SORT =========================
-
-// function insertion sort dengan parameter array, awal, akhir dan balikan berupa slice int
-func InsertionSort(arr []int, i, j int) []int {
-	// jika awal lebih kecil dari akhir
-	if i < j {
-		// tetapkan nilai awal
-		k := i
-		// rekursif
-		InsertionSort(arr, k+1, j)
-		// lakukan penggabungan
-		Merge(arr, i, k, j)
-	}
-
-	return arr
-}
-
-// function merge dengan parameter array, awal, tengah, akhir dan balikan berupa slice int
-func Merge(arr []int, i, k, j int) []int {
-	// merge hasil pembagian
-	for i <= k && k <= j {
-		// jika nilai awal lebih besar dari nilai tengah
-		if arr[i] > arr[k] {
-			// lakukan pertukaran posisi
-			arr[i], arr[k] = arr[k], arr[i]
-			// increment i dan k
-			i++
-			k++
-		} else {
-			// incerment k
-			k++
-		}
-	}
-	// kembalikan array
-	return arr
-}
-
-// =================== END INSERTION SORT =====================
-
 func main() {
-
 	// penghitung waktu
 	start := time.Now()
 
@@ -130,25 +81,6 @@ func main() {
 
 	// print data setelah diurutkan
 	fmt.Printf("Data setelah diurutkan: %v\n\n\n\n", data)
-
-	// durasi ekseskusi program
-	fmt.Printf("Durasi eksekusi program: %v Detik \n", time.Since(start).Seconds())
-
-	fmt.Printf("\n==============================================================\n")
-	fmt.Println("====================== INSERTION SORT ========================")
-	fmt.Printf("==============================================================\n")
-
-	// my data
-	data2 := ReadData("Number.txt")
-
-	// print data sebelum diurutkan
-	fmt.Printf("Data sebelum diurutkan: %v\n", data2)
-
-	// urutkan data
-	res := InsertionSort(data2, 0, len(data2)-1)
-
-	// print data setelah diurutkan
-	fmt.Printf("Data setelah diurutkan: %v\n\n\n\n", res)
 
 	// durasi ekseskusi program
 	fmt.Printf("Durasi eksekusi program: %v Detik \n", time.Since(start).Seconds())
